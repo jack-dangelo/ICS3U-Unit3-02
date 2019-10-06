0063ff89fd1bf2bac15c25b37a36c258c87ff843
jack-dangelo <54954654+jack-dangelo@users.noreply.github.com>
2019-10-06 19:27:10 -0400

    Create number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Jack D'Angelo
+# Created on: September 2019
+# This program is a number guessing game
+
+
+def main():
+    # This function does plays a game
+    # Input
+    number_guessed = int(input("Enter a number to play: "))
+    print(" ")
+    # Process
+    if number_guessed == 5:
+        print("Congrats you are cool.")
+    else:
+        print("You are wrong, try again?.")
+
+
+if __name__ == "__main__":
+    main()
